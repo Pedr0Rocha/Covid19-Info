4f69af9218fc899f7838ee790086c9fac72650d9
Pedro Rocha <pedrovqrocha@gmail.com>
2020-05-24 20:59:09 -0300

    Creating project

---
+++ app/src/main/java/com/pedrorocha/covid19info/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pedrorocha.covid19info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pedrorocha.covid19info.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
