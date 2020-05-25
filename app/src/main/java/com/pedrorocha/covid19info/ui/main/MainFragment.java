b2bceed6e3ec8f6d7a7c138890d64d5087c836db
Pedro Rocha <pedrovqrocha@gmail.com>
2020-05-24 22:39:15 -0300

    Added Databinding

--- app/src/main/java/com/pedrorocha/covid19info/ui/main/MainFragment.java
+++ app/src/main/java/com/pedrorocha/covid19info/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package com.pedrorocha.covid19info.ui.main;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -7,16 +8,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pedrorocha.covid19info.R;
+import com.pedrorocha.covid19info.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+
+    private MainFragmentBinding binding;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -26,7 +31,15 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+        binding = DataBindingUtil.inflate(inflater,
+                R.layout.main_fragment,
+                container,
+                false);
+
+        binding.setLifecycleOwner(this);
+
+        return binding.getRoot();
     }
 
     @Override
