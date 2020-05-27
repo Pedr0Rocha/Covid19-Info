74bf4106ea9a3d5daa533bf020ece9e607312051
Pedro Rocha <pedrovqrocha@gmail.com>
2020-05-26 22:11:48 -0300

    Added Dagger2, Network and App modules

--- app/src/main/java/com/pedrorocha/covid19info/utils/AppConstants.java
+++ app/src/main/java/com/pedrorocha/covid19info/utils/AppConstants.java
@@ -2,6 +2,7 @@ package com.pedrorocha.covid19info.utils;
 
 public class AppConstants {
 
+    public static String API_BASE_URL = "https://api.covid19api.com/";
 
     public static String BUNDLE_COUNTRY_SLUG = "country_slug";
 
